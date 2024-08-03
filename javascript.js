91333ef2a8899ef569587432e3448adbc1b071fc
philip <bigtea@Xubuntu.myguest.virtualbox.org>
2024-08-03 22:44:47 +0200

    adding functionality of drawing in main

--- javascript.js
+++ javascript.js
@@ -34,9 +34,27 @@ for (let i = 0; i < 8; i++) {
     const div=document.createElement("div");
     div.classList.add("maincolorblock");
     main.appendChild(div);
+
+    let isMouseDown = false;
+
     for (let j = 0; j < 8; j++) {
         const feld = document.createElement("div");
         feld.style.cssText = "background-color:white; width:50px; height:50px; border-color:black; border-width:5px;";
         div.appendChild(feld);
+        feld.addEventListener('click', () =>{
+            feld.style.backgroundColor="blue";
+        })
+
+        feld.addEventListener('mousedown', () =>{
+            isMouseDown = true;
+        })
+        feld.addEventListener('mouseup', () =>{
+            isMouseDown = false;
+        })
+        feld.addEventListener('mouseenter', ()=>{
+            if (isMouseDown){
+                feld.style.backgroundColor='blue';
+            }
+        })
     }    
 }
